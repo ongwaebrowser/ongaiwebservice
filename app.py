d2c7314f423ebddb94322c10926c8f09b6fb71f4
ongwaebrowser <ongwaebrowser7@gmail.com>
2024-10-06 16:35:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def query():
         answer = markdown2.markdown(data['answer'])
         return jsonify({'response': answer})
     else:
-        return jsonify({'response': "Dear user, some error occurred, either the server is busy. Relax, Have no worry, You can you can prefer trying asking again!😊."})
+        return jsonify({'response': "retry again."})
 
 if __name__ == '__main__':
     app.run(debug=True)
